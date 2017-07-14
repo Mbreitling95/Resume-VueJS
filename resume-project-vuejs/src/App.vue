aaa8e1858075f7e2759db9051635aa8d1b0ed9e7
Matthew Breitling <MBreitling95@gmail.com>
2017-07-13 19:22:49 -0500

    Edits, not including .editorconfig change yet

--- resume-project-vuejs/src/App.vue
+++ resume-project-vuejs/src/App.vue
@@ -1,8 +1,28 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <p>Current Page: {{ $route.name }}</p>
+    
+    <p>
+      <router-link to="/">Go to Home</router-link>
+      <router-link to="/world">Go to World</router-link>
+
+    </p>
+
     <router-view></router-view>
   </div>
+
+
+  <!-- <div id="app">
+      <h1>Named Routes</h1>
+      <p>Current route name: {{ $route.name }}</p>
+      <ul>
+        <li><router-link :to="{ name: 'home' }">home</router-link></li>
+        <li><router-link :to="{ name: 'foo' }">foo</router-link></li>
+        <li><router-link :to="{ name: 'bar', params: { id: 123 }}">bar</router-link></li>
+      </ul>
+      <router-view class="view"></router-view>
+    </div> -->
 </template>
 
 <script>
--- resume-project-vuejs/src/router/index.js
+++ resume-project-vuejs/src/router/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import World from '@/components/World'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
-  ]
+const routes = [
+   {path: '/', name: 'Hello', component: Hello},
+   {path: '/World', name: 'World', component: World}
+]
+
+const router = new Router({
+  routes: routes
 })
+
+export default router
